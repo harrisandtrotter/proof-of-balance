27f4c63e36d34046d21ca8980868b5f6702440b8
chrisorei <chrisoreilly@harrisandtrotter.co.uk>
2023-09-18 20:00:31 +0100

    further import errors rectified

--- server/blocks/blocks.go
+++ server/blocks/blocks.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/harrisandtrotter/proof-of-balance/backend/initialisers"
+	"github.com/harrisandtrotter/proof-of-balance/server/initialisers"
 )
 
 type Block struct {
